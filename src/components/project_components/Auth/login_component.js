1e0378ff8cc2780df8dff2e93e7fbc7e4e460d43
GaraziLS <glabakasanabria@gmail.com>
2024-09-18 17:32:13 +0200

    debugging

--- src/components/project_components/Auth/login_component.js
+++ src/components/project_components/Auth/login_component.js
@@ -39,6 +39,7 @@ export default class LoginComponent extends Component {
             .then(response => {
                 console.log("Full Response:", response); // Log the whole response object
                 console.log("Response Data:", response.data); // Log just the response data
+                console.log("Login Successful")
                 if (response.status === 200) {
                     this.props.handleSuccessfulLoginAuth();
                 }
