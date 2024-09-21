41e6e0ea41e870b16d26f0f710aa53ca73e196a8
GaraziLS <glabakasanabria@gmail.com>
2024-09-21 11:20:22 +0200

    test with HOC

--- src/components/project_components/Auth/login-helper.js
+++ src/components/project_components/Auth/login-helper.js
@@ -1,10 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 
-function LoginHelper(WrappedComponent) {
-    return function(props) {
-        const navigate = useNavigate(); // Hook called inside the functional component
+function LoginHelper(props) {
+    const navigate = useNavigate(); // Hook called inside the functional component
         return <WrappedComponent {...props} navigate={navigate} />; // Pass `navigate` to the wrapped component
     };
-}
 
 export default LoginHelper;
